14a282a60753da3d209dd16263c984b647ed96f1
Dvasrn <dawaasurenbazarragcha4@gamil.com>
2024-07-30 17:51:50 +0800

    camera

--- utils/sheetDataProcess.ts
+++ utils/sheetDataProcess.ts
@@ -1,5 +1,16 @@
-export const sheetdataProcess = async (values: any) => {
-  console.log("sheetdataProcess", values);
-  
-  return [];
+export const sheetdataProcess = async (data: any) => {
+  console.log("sheetdataProcess", data);
+  const keys = data[0];
+  const values = [data[1], data[2], data[3]];
+  // keys.map((key) => {
+  //   key
+  // })
 };
+
+// function clearInvoice() {
+//   const sheet = SpreadsheetApp.getActiveSheet();
+//   const invoiceNumber = sheet.getRange("B5").clearContent();
+//   const invoiceAmount = sheet.getRange("B8").clearContent();
+//   const invoiceTo = sheet.getRange("E5").clearContent();
+//   const invoiceFrom = sheet.getRange("E6").clearContent(); 
+// }
